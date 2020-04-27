551a8e2d01bbff6028c668cf4b01f89bcc7424cb
gabriellatrujillor <gabriella.trujillor@gmail.com>
2020-04-26 21:55:05 -0600

    text area is green, showing on localstorage

--- Develop/javascript.js
+++ Develop/javascript.js
@@ -1,13 +1,10 @@
 // Create Calendar/Planner
-createCal();
 console.log(moment().format("dddd, MMMM Do YYYY h:mm HH"));
 var currDay = moment().format("dddd, MMMM Do YYYY h:mm HH");
 console.log(currDay);
 var currHour = parseInt(currDay.split(" ")[4].split(":")[0]);
 console.log("Current hour: " + currHour);
-
-
-function createCal() {
+var getList = localStorage.getItem("To-do-list")
    var timeArray = [
     9,
     10,
@@ -26,36 +23,27 @@ function createCal() {
     23,
     24,
   ];
-  
+  var userArray = ["", "", "", "", "", "", "", "", "",];
   //   Create elements on page
   for (var i = 0; i < 9; i++) {
     var div1 = $("<div>");
     div1.attr("class", "input-group time-block");
-    //<div class="input-group"></div>
     var div2 = $("<div>");
     div2.attr("class", "input-group-prepend hour");
-    //<div class="input-group-prepend"></div>
     var span = $("<span>");
     span.attr("class", "input-group-text");
-    //<span class="input-group-text" ></span>
     span.attr("id", "time");
-    //<span class="input-group-text" id="time"></span>
     var textArea = $("<textarea>");
-    textArea.attr("class", "form-control row description");
-    //<textarea class="form-control"></textarea>
+    textArea.attr("class", "form-control row description past");
     textArea.attr("aria-label", "With textarea");
-    //<textarea class="form-control" aria-label="With textarea"></textarea>
     textArea.attr("id", `${i}`);
-    //<textarea class="form-control" aria-label="With textarea" id="time-val"></textarea>
     var div3 = $("<div>");
     div3.attr("class", "input-group-append").value;
-    //<div class="input-group-append"></div>
     var btn = $("<button>");
     btn.attr("class", "btn btn-outline-secondary saveBtn");
     btn.attr("type", "button");
     btn.attr("id", `${i}`);
     btn.text("Save");
-    //<button class="btn btn-outline-secondary" type="button" id="button-addon2 time-btn">Submit</button>
     // Append divs:
     div2.append(span);
     div3.append(btn);
@@ -74,32 +62,28 @@ function createCal() {
     }
     $(".container").append(div1);
   }
-
+//   DISPLAY DATE HEADER
   var displayDay = moment().format("MMM Do");
   console.log(displayDay)
-    $("#currentDay").append(displayDay);
-
-
-  //   Save button event listener - NOT WORKING
-  $("button").on("click", function () {
-    // first, assign a unique id to the save button because $(this) will only return the save button
-    console.log($(this).attr("id"));
-    // using the unique id find the corrisponding textarea and assign a variable to obtain the value
-    console.log("click");
-    function saveInput() {
-      localStorage.setItem("input", textArea.val());
-    }
-    //saveInput()
+  $("#currentDay").append(displayDay);
+// *******THIS CODE IS WORKING, WILL DISPLAY TEXT AREA IN LOCALSTORAGE BUT NOT GOING THROUGH EACH BUTTON
+$("button").on("click", function () {
+    var info = $(this).parent("div").prev().val();
+    console.log(info);
+    localStorage.setItem("userArray", JSON.stringifY(userArray));
   });
+// var futureTime = currHour +;
+// console.log(futureTime + "time");
+// **********
   //   Time blocks change depending on current time - NOT WORKING
-  function blockColor() {
-    if (timeArray[i] < currHour) {
-      textArea.addClass("past");
-    } else if (timeArray[i] > currHour) {
-      textArea.addclass("future");
-    } else {
-      textArea.addClass("present");
-    }
+  function futureBlockColor() {
+    var futureTime = currHour + 1 
+  //   var futureColor = $("textarea").css("background-color", "green")
+    console.log(futureTime + "time");
+    for (var i = futureTime; i > currHour;  ) {
+        var futureColor = $("textarea").css("background-color", "green")
+      textarea.classList.add(futureColor);
+      }
   }
-}
+  futureBlockColor();
 
